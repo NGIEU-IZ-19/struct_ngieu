3af0550a207994181f2e138c585faae67803c2de
Шатов Илья Андреевич <shatovia@ict29.ru>
2020-10-28 21:52:03 +0300

    Дополнение функционала функции show_route, добавление функции lineSearch

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ struct MARSH // Объявляем структуру MARSH
 {
 	std::string BEGST;	// данные беруться из стандартного потока ввода stdin (консоль)
 	std::string TERM;	// данные беруться из стандартного потока ввода stdin (консоль)
-	std::string	NUMER;
+	float NUMER;	// данные беруться из стандартного потока ввода stdin (консоль)
 };
 
 void add_route(MARSH TRAFIC[], int arraylength) // разобраться
@@ -22,35 +22,69 @@ void add_route(MARSH TRAFIC[], int arraylength) // разобраться
 		std::cout << "Название конечного пункта маршрута: "; 
 		std::cin >> TRAFIC[i].TERM;
 	}
+
 	std::cout << std::endl; // завершить вывод из стандартного потока вывода stdout (консоль), т.е. (?)
 }
 
-void show_route(MARSH TRAFIC[]) // разобраться
+
+//линейный поиск
+int lineSearch(MARSH TRAFIC[], int requiredKey, int arraylength)
 {
-	int i;
-	std::cout << "Введите с клавиатуры номер маршрута: "; // разобратиься с описанием (передать на вывод данные обработчику ввода вывода)
-	std::cin >> i;
-	std::cout << "Данные по вашему запросу" << std::endl;
-	std::cout << "Название начального пункта маршрута: " << TRAFIC[i-1].BEGST << std::endl;
-	std::cout << "Название конечного пункта маршрута: " << TRAFIC[i-1].TERM << std::endl;
-	std::cout << "Номер маршрута" << TRAFIC[i-1].NUMER << std::endl;
-	std::cout << std::endl; // завершить вывод из стандартного потока вывода stdout (консоль), т.е. (?)
+
+		for (int i = 0; i < arraylength; i++)
+		{
+			if (TRAFIC[i].NUMER == requiredKey)
+				return i ;
+		}
+
+		return 0;
+
 }
 
 
-int main (void)							// Объявляем главную функцию которая возвращает целое число при выполнении
+
+void show_route(MARSH TRAFIC[], int arraylength) // разобраться
+{
+
+	float requiredKey;
+	MARSH n = { "none", "none" };
+	
+	std::cout << "Введите с клавиатуры номер маршрута: "; // разобратиься с описанием (передать на вывод данные обработчику ввода вывода)
+	std::cin >> requiredKey;
+
+	int m = (lineSearch(TRAFIC, requiredKey, arraylength));
+
+	if (m == 0)
+	{
+		std::cout << "Маршрут отсутствует в массиве" << std::endl;
+	}
+
+	else 
+	
+	{ 
+		std::cout << "Данные по вашему запросу:" << std::endl;
+		std::cout << "Номер маршрута" << TRAFIC[m].NUMER << std::endl;
+		std::cout << "Название начального пункта маршрута: " << TRAFIC[m].BEGST << std::endl;
+		std::cout << "Название конечного пункта маршрута: " << TRAFIC[m].TERM << std::endl;
+		
+	}
+
+	std::cout << std::endl;			// завершить вывод из стандартного потока вывода stdout (консоль), т.е. (?)
+}
+
+
+int main (void)						// Объявляем главную функцию которая возвращает целое число при выполнении (?)
 {
 	//Блок объявления переменных
 	const int arraylength = 8;
-
 	MARSH TRAFIC[arraylength];
 	//Блок объявления переменных
 
-	setlocale(LC_ALL, "rus"); // установка вывода локации
+	setlocale(LC_ALL, "rus");		// установка вывода локации
 	add_route(TRAFIC, arraylength); // вызов метода (функции), передача переменных в качестве входных параметров
-	show_route(TRAFIC);
-	system("pause");		 // остановка выполнения программы
-	return 0;				//  вывод  
+	show_route(TRAFIC, arraylength);			//
+	system("pause");				// остановка выполнения программы
+	return 0;						//  вывод  
 }
 
 
