9132e2216affacf339606624bdf3fdf8d241da11
Шатов Илья Андреевич <shatovia@ict29.ru>
2020-10-29 11:50:50 +0300

    Добавление функции сортировки, функции просмотра всего массива маршрутов

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ struct MARSH // Объявляем структуру MARSH
 {
 	std::string BEGST;	// данные беруться из стандартного потока ввода stdin (консоль)
 	std::string TERM;	// данные беруться из стандартного потока ввода stdin (консоль)
-	float NUMER;	// данные беруться из стандартного потока ввода stdin (консоль)
+	int NUMER;	// данные беруться из стандартного потока ввода stdin (консоль)
 };
 
-void add_route(MARSH TRAFIC[], int arraylength) // разобраться
+void add_route(MARSH TRAFIC[], int arraylength) // Функция для добавления  данный в массив маршрутов, с последующей сортировкой
 {
 	std::cout << "Введите с клавиатуры информации для заполнения массива " << std::endl; // разобратиься с описанием (передать на вывод данные обработчику ввода вывода)
 	int i;
@@ -23,30 +23,49 @@ void add_route(MARSH TRAFIC[], int arraylength) // разобраться
 		std::cin >> TRAFIC[i].TERM;
 	}
 
-	std::cout << std::endl; // завершить вывод из стандартного потока вывода stdout (консоль), т.е. (?)
+	TRAFIC[arraylength] = ((sort_increase(TRAFIC, arraylength)));
+
 }
 
 
-//линейный поиск
-int lineSearch(MARSH TRAFIC[], int requiredKey, int arraylength)
+MARSH sort_increase(MARSH TRAFIC[], int arraylength) // Функция для сортировки элементво массива методом "пузырков"
 {
+	int i;
+	int r;
+	MARSH arr_temp;
 
+	for (i = 0; i < unsigned(arraylength); i++)
+	{
+	
+		for (r = 0; r < unsigned(arraylength); r++) 
+		{
+			// Обмен местами по принципу более легкий 
+			if (TRAFIC[r].NUMER > TRAFIC[r+1].NUMER)
+			{ 
+				arr_temp = TRAFIC[r+1];
+				TRAFIC[r + 1] = TRAFIC[r];
+				TRAFIC[r] = arr_temp;
+			}
+		}
+	}
+	return TRAFIC[arraylength];
+
+}
+
+int lineSearch(MARSH TRAFIC[], int requiredKey, int arraylength)			//линейный поиск
+{
 		for (int i = 0; i < arraylength; i++)
 		{
 			if (TRAFIC[i].NUMER == requiredKey)
 				return i ;
 		}
-
 		return 0;
-
 }
 
-
-
-void show_route(MARSH TRAFIC[], int arraylength) // разобраться
+void show_one_route(MARSH TRAFIC[], int arraylength) // Функция не возвращающая значений для просмотра определенного маршрута
 {
 
-	float requiredKey;
+	int requiredKey;
 	MARSH n = { "none", "none" };
 	
 	std::cout << "Введите с клавиатуры номер маршрута: "; // разобратиься с описанием (передать на вывод данные обработчику ввода вывода)
@@ -58,20 +77,29 @@ void show_route(MARSH TRAFIC[], int arraylength) // разобраться
 	{
 		std::cout << "Маршрут отсутствует в массиве" << std::endl;
 	}
-
 	else 
-	
 	{ 
 		std::cout << "Данные по вашему запросу:" << std::endl;
 		std::cout << "Номер маршрута" << TRAFIC[m].NUMER << std::endl;
 		std::cout << "Название начального пункта маршрута: " << TRAFIC[m].BEGST << std::endl;
 		std::cout << "Название конечного пункта маршрута: " << TRAFIC[m].TERM << std::endl;
-		
 	}
 
-	std::cout << std::endl;			// завершить вывод из стандартного потока вывода stdout (консоль), т.е. (?)
 }
 
+
+void show_full_route(MARSH TRAFIC[], int arraylength) // Функция не возвращающая значений для просмотра определенного маршрута
+{
+
+	int i;
+	for (i = 0; i < unsigned(arraylength); i++)
+	{
+		std::cout << "Номер маршрута" << TRAFIC[i].NUMER << std::endl;
+		std::cout << "Название начального пункта маршрута: " << TRAFIC[i].BEGST << std::endl;
+		std::cout << "Название конечного пункта маршрута: " << TRAFIC[i].TERM << std::endl;
+	}
+
+}
 
 int main (void)						// Объявляем главную функцию которая возвращает целое число при выполнении (?)
 {
@@ -82,12 +110,8 @@ int main (void)						// Объявляем главную функцию которая возвращает целое число
 
 	setlocale(LC_ALL, "rus");		// установка вывода локации
 	add_route(TRAFIC, arraylength); // вызов метода (функции), передача переменных в качестве входных параметров
-	show_route(TRAFIC, arraylength);			//
+	show_one_route(TRAFIC, arraylength);
+	show_full_route(TRAFIC, arraylength);
 	system("pause");				// остановка выполнения программы
 	return 0;						//  вывод  
 }
-
-
-
-
-
